a3de0979c6982fd1ed11a472ce096678ff04e288
Soner Akça <167956467+ultrasoner55@users.noreply.github.com>
2026-03-04 02:17:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,33 +2,48 @@ const channelData = [
   {
     category: "Ulusal Haber",
     channels: [
+      { name: "1-TRT Haber", url: "https://tv-trt1.medya.trt.com.tr/master.m3u8" },
+      { name: "2-A Haber", url: "https://trkvz-live.daioncdn.net/ahaber/ahaber.m3u8" },
+      { name: "3-Haber Global", url: "https://tv.ensonhaber.com/haberglobal/haberglobal.m3u8" },
+      { name: "4-TV100", url: "https://tv100-live.ercdn.net/tv100/tv100.m3u8" },
+      { name: "5-Bloomberg HT", url: "https://bloomberght.live.cdn.bitgravity.com/cdn-live/stream.m3u8" }
+      { name: "6-TRT Spor", url: "https://tv-trtspor1.medya.trt.com.tr/master.m3u8" },
+      { name: "7-TRT Spor Yıldız", url: "https://tv-trtspor2.medya.trt.com.tr/master.m3u8" },
+      { name: "8-A Spor", url: "https://trkvz-live.daioncdn.net/aspor/aspor.m3u8" },
+      { name: "9-S Sport Plus Tanıtım", url: "https://ssportplusmobilehls.ercdn.net/SSportPlus/movie.m3u8" }
+      { name: "10-TRT Belgesel", url: "https://tv-trtbelgesel.medya.trt.com.tr/master.m3u8" },
+      { name: "NASA TV", url: "https://ntv1-lh.akamaihd.net/i/NASA_101@319270/master.m3u8" },
+      { name: "TRT EBA TV", url: "https://tv-eba.medya.trt.com.tr/master.m3u8" }
+      { name: "Kral Pop TV", url: "https://dogus-live.daioncdn.net/kralpop/kralpop.m3u8" },
+      { name: "Dream Türk", url: "https://dogus-live.daioncdn.net/dreamturk/dreamturk.m3u8" },
+      { name: "PowerTürk TV", url: "https://livetv.powerapp.com.tr/powerturkTV/powerturkhd.smil/chunklist_w650000.m3u8" }
       { name: "TRT Haber", url: "https://tv-trt1.medya.trt.com.tr/master.m3u8" },
       { name: "A Haber", url: "https://trkvz-live.daioncdn.net/ahaber/ahaber.m3u8" },
       { name: "Haber Global", url: "https://tv.ensonhaber.com/haberglobal/haberglobal.m3u8" },
       { name: "TV100", url: "https://tv100-live.ercdn.net/tv100/tv100.m3u8" },
       { name: "Bloomberg HT", url: "https://bloomberght.live.cdn.bitgravity.com/cdn-live/stream.m3u8" }
-    ]
-  },
-  {
-    category: "Spor Canlı",
-    channels: [
       { name: "TRT Spor", url: "https://tv-trtspor1.medya.trt.com.tr/master.m3u8" },
       { name: "TRT Spor Yıldız", url: "https://tv-trtspor2.medya.trt.com.tr/master.m3u8" },
       { name: "A Spor", url: "https://trkvz-live.daioncdn.net/aspor/aspor.m3u8" },
       { name: "S Sport Plus Tanıtım", url: "https://ssportplusmobilehls.ercdn.net/SSportPlus/movie.m3u8" }
-    ]
-  },
-  {
-    category: "Belgesel & Bilim",
-    channels: [
       { name: "TRT Belgesel", url: "https://tv-trtbelgesel.medya.trt.com.tr/master.m3u8" },
       { name: "NASA TV", url: "https://ntv1-lh.akamaihd.net/i/NASA_101@319270/master.m3u8" },
       { name: "TRT EBA TV", url: "https://tv-eba.medya.trt.com.tr/master.m3u8" }
-    ]
-  },
-  {
-    category: "Eğlence & Müzik",
-    channels: [
+      { name: "Kral Pop TV", url: "https://dogus-live.daioncdn.net/kralpop/kralpop.m3u8" },
+      { name: "Dream Türk", url: "https://dogus-live.daioncdn.net/dreamturk/dreamturk.m3u8" },
+      { name: "PowerTürk TV", url: "https://livetv.powerapp.com.tr/powerturkTV/powerturkhd.smil/chunklist_w650000.m3u8" }
+      { name: "TRT Haber", url: "https://tv-trt1.medya.trt.com.tr/master.m3u8" },
+      { name: "A Haber", url: "https://trkvz-live.daioncdn.net/ahaber/ahaber.m3u8" },
+      { name: "Haber Global", url: "https://tv.ensonhaber.com/haberglobal/haberglobal.m3u8" },
+      { name: "TV100", url: "https://tv100-live.ercdn.net/tv100/tv100.m3u8" },
+      { name: "Bloomberg HT", url: "https://bloomberght.live.cdn.bitgravity.com/cdn-live/stream.m3u8" }
+      { name: "TRT Spor", url: "https://tv-trtspor1.medya.trt.com.tr/master.m3u8" },
+      { name: "TRT Spor Yıldız", url: "https://tv-trtspor2.medya.trt.com.tr/master.m3u8" },
+      { name: "A Spor", url: "https://trkvz-live.daioncdn.net/aspor/aspor.m3u8" },
+      { name: "S Sport Plus Tanıtım", url: "https://ssportplusmobilehls.ercdn.net/SSportPlus/movie.m3u8" }
+      { name: "TRT Belgesel", url: "https://tv-trtbelgesel.medya.trt.com.tr/master.m3u8" },
+      { name: "NASA TV", url: "https://ntv1-lh.akamaihd.net/i/NASA_101@319270/master.m3u8" },
+      { name: "TRT EBA TV", url: "https://tv-eba.medya.trt.com.tr/master.m3u8" }
       { name: "Kral Pop TV", url: "https://dogus-live.daioncdn.net/kralpop/kralpop.m3u8" },
       { name: "Dream Türk", url: "https://dogus-live.daioncdn.net/dreamturk/dreamturk.m3u8" },
       { name: "PowerTürk TV", url: "https://livetv.powerapp.com.tr/powerturkTV/powerturkhd.smil/chunklist_w650000.m3u8" }
