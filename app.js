dc802419c98779a4b54cc8c46cc752a76bf4fec1
Soner Akça <167956467+ultrasoner55@users.noreply.github.com>
2026-03-04 05:31:18 +0300

    Fix missing commas in TV channel URLs
    Fixed missing commas in the channel list.

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ const channelData = [
       { name: "2•TRT HABER HD", url: "https://tv-trthaber.medya.trt.com.tr/master.m3u8" },
       { name: "3•TRT SPOR HD", url: "https://tv-trtspor1.medya.trt.com.tr/master.m3u8" },
       { name: "4•TRT TÜRK HD", url: "https://tv-trtturk.medya.trt.com.tr/master.m3u8" },
-      { name: "5•TRT MÜZİK HD", url: "https://tv-trtmuzik.medya.trt.com.tr/master.m3u8" }, // Buradaki virgül eksikti
+      { name: "5•TRT MÜZİK HD", url: "https://tv-trtmuzik.medya.trt.com.tr/master.m3u8" },
       { name: "6•TRT BELGESEL HD", url: "https://tv-trtbelgesel.medya.trt.com.tr/master.m3u8" },
       { name: "7•SHOW TV", url: "https://tv-trtspor2.medya.trt.com.tr/master.m3u8" },
       { name: "8•SHOW TÜRK", url: "https://trkvz-live.daioncdn.net/aspor/aspor.m3u8" },
-      { name: "9•KANAL D", url: "https://ssportplusmobilehls.ercdn.net/SSportPlus/movie.m3u8" }, // Buradaki virgül eksikti
+      { name: "9•KANAL D", url: "https://ssportplusmobilehls.ercdn.net/SSportPlus/movie.m3u8" },
       { name: "10•EURO D", url: "https://tv-trtbelgesel.medya.trt.com.tr/master.m3u8" },
       { name: "11•ATV", url: "https://trkvz.daioncdn.net/atv/atv_1080p.m3u8?e=1772635715&st=H5NUj0b0gUZTydSzdjQKqw&sid=87rw3sbzvpnn&app=d1ce2d40-5256-4550-b02e-e73c185a314e&ce=3" },
-      { name: "12•ATV AVRUPA", url: "https://tv-eba.medya.trt.com.tr/master.m3u8" }, // Buradaki virgül eksikti
+      { name: "12•ATV AVRUPA", url: "https://tv-eba.medya.trt.com.tr/master.m3u8" },
       { name: "13•NTV", url: "https://dogus-live.daioncdn.net/kralpop/kralpop.m3u8" },
       { name: "14•DMAX", url: "https://dogus-live.daioncdn.net/dreamturk/dreamturk.m3u8" },
       { name: "15•NOW TV", url: "https://livetv.powerapp.com.tr/powerturkTV/powerturkhd.smil/chunklist_w650000.m3u8" }
